ff7bcae0e4b70345650ea352649481fbeee842af
Sean Lee <81959287+Xalph555@users.noreply.github.com>
2022-02-17 20:20:10 +1100

    UpdateFixes

--- Map/RestoreAmmo.gd
+++ Map/RestoreAmmo.gd
@@ -17,6 +17,18 @@ func _ready() -> void:
 
 
 func _on_body_entered(body: Node) -> void:
+	var player = body as Player
+		
+	if not player:
+		if body is Car:
+			player = body.player_reference as Player
+			
+			if not player:
+				return
+		
+		else:
+			return
+		
 	PlayerStats.poster_ammo += PlayerStats.base_poster_ammo - PlayerStats.poster_ammo
 	PlayerStats.egg_ammo += PlayerStats.base_egg_ammo - PlayerStats.egg_ammo
 	_particles.emitting = true
